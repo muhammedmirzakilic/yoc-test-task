ec2d762d8fb618476bb4952175ff4910941d8856
Mirza <muhammedmirzakilic@gmail.com>
2022-02-11 17:32:05 +0300

    autoplay video when isVisible

--- src/components/VideoPlayer/index.tsx
+++ src/components/VideoPlayer/index.tsx
@@ -1,9 +1,9 @@
-import { SyntheticEvent, useRef, useState } from "react";
-
+import { SyntheticEvent, useEffect, useRef, useState } from "react";
+import { useIsVisible } from "./hooks";
 const VideoPlayer = (props: any) => {
   const [progress, setProgress] = useState(0);
   const videoRef = useRef<HTMLVideoElement>(null);
-
+  const isVisible = useIsVisible(videoRef, props.threshold);
   const handleOnTimeUpdate = (e: SyntheticEvent<HTMLVideoElement>) => {
     //I don't know if the progress  percentage is required to be strictly correct or not.
     //If it is, progress should be calculated with different method(like using requestAnimationFrame).
@@ -11,6 +11,15 @@ const VideoPlayer = (props: any) => {
     const time = Math.round((currentTime / duration) * 100);
     setProgress(time);
   };
+  useEffect(() => {
+    if (!videoRef || !videoRef.current) return;
+    if (isVisible) {
+      videoRef.current.play();
+    } else {
+      videoRef.current.pause();
+    }
+    return () => {};
+  }, [isVisible]);
   return (
     <video
       width="100%"
@@ -18,6 +27,7 @@ const VideoPlayer = (props: any) => {
       ref={videoRef}
       onTimeUpdate={handleOnTimeUpdate}
       autoPlay={false}
+      muted={true} // to be able to autoplay video
     >
       <source src={props.src} type="video/mp4" />
     </video>
